073ac5bc742e3dbd0f7b971b22fc5741fdf7c562
Orange4Broom <orang4broom@gmail.com>
2024-12-04 00:42:30 +0100

    Adding stylings for the navigation and adding meta tags for ios add to home screen

--- src/components/blocks/navigation/Navigation.tsx
+++ src/components/blocks/navigation/Navigation.tsx
@@ -1,15 +1,16 @@
 import { NavigationSearch } from '@elements/navigationSearch/NavigationSearch';
 import { FC } from 'react';
+import './navigation.scss';
 
 export const Navigation: FC = () => {
   return (
     <nav className='navigation'>
       <form className='navigation__content' onSubmit={() => console.log("bruh")}>
-        <button className='navigation__submit'>
+        <button className='navigation__alcoholic' type='submit'>
           Alkoholické
         </button>
         <NavigationSearch />
-        <button className='navigation__submit'>
+        <button className='navigation__submit' type='submit'>
           Hledat
         </button>
       </form>
